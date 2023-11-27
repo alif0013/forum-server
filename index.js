6ae27cd5eb366cb545229dda7cde742358727606
alif0013 <mdalifh36@gmail.com>
2023-11-28 00:23:33 +0600

    added post method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -34,6 +34,25 @@ async function run() {
             const result = await postsCollection.find().toArray();
             res.send(result)
         })
+
+          // find a single data 
+        //   app.get('/posts/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: new ObjectId(id) }
+        //     const result = await postsCollection.findOne(query)
+        //     res.send(result)
+        // })
+
+     // Add post to the database
+        app.post('/posts', async (req, res) => {
+            const newPost = req.body;
+            // console.log(newAsignment);
+            const result = await postsCollection.insertOne(newPost)
+            res.send(result)
+        })
+
+
+
 
 
         // Send a ping to confirm a successful connection
